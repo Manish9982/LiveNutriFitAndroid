bfd37b7cb1618ee2897a739601e58b564dd2e6b8
Gaurav <gauravch@outsourcingservicesusa.com>
2023-12-08 11:21:41 +0530

    First Commit

---
+++ react-native.config.js
@@ -0,0 +1,15 @@
+module.exports = {
+  dependencies: {
+    'react-native-vector-icons': {
+      platforms: {
+        ios: null,
+      },
+    },
+  },
+    project: {
+      
+      android: {},
+    },
+    assets: ['./src/assets/fonts/Montserrat/static'],
+  
+};
